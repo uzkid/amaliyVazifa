978d3c4927b642353afea65df11f949757c67e60
uzkid <ibowik694@gmail.com>
2024-08-07 13:23:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+Console.Write("Lets Convert Your Dollar to Sum Enter how much dollars you Have:");
+
+int ConvertedDollar = System.Convert.ToInt32(Console.ReadLine());
+
+ConvertedDollar *= 12400;
+System.Console.WriteLine(ConvertedDollar);
+
+int ConvertedSum = ConvertedDollar;
+Console.WriteLine($"converted your dollars to {ConvertedSum} sums");
